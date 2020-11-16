3fe8203140346f52b5c79073f6f433ba1d9caea9
Lucas <liger.lucas33@gmail.com>
2020-11-16 22:32:16 +0100

    j'avais rien à faire du coup j'ai rajouté 2 3 truc

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,11 @@ Button.addEventListener('click', commande);
 const Inventory = document.getElementById('inventaire');
 
 const crossButton = document.getElementById('crossButton');
-crossButton.addEventListener('click', () => Inventory.style.display ='none')
+crossButton.addEventListener('click', () => Inventory.style.display = 'none')
+
+//Variables de déplacements
+let x = 0;
+let y = 0;
 
 //Fonction cotenant le switch de toutes les commandes disponibles avec l'input
 function commande() {
@@ -17,11 +21,21 @@ function commande() {
 
         //Permet d'ouvrir l'inventaire
         case 'inventaire':
-            Inventory.style.display ='block';
+            Inventory.style.display = 'block';
             break;
 
-            
+        case 'droite':
+            window.alert('Vous allez tourner à droite !')
+            x++;
+            localStorage.setItem('x', x);
+            break;
+        case 'gauche':
+            window.alert('Vous allez tourner à gauche !')
+            x--;
+            localStorage.setItem('x', x);
+            break;
         default:
+            window.alert('Veuillez rentrer une commande valide.')
             break;
     }
 
